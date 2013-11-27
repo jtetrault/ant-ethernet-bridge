1b3d21f2e19c60e7818e2d31e120ae34d0209a60
Joel Tetrault <star99ers@gmail.com>
2013-11-26 23:49:56 -0600

    create standalone instance method for ANTListener RxDelegate

--- ANTBridge/ANTBridge/ANTBridge.cs
+++ ANTBridge/ANTBridge/ANTBridge.cs
@@ -16,7 +16,21 @@ namespace ANTBridge
         /// </summary>
         public ANTBridge()
         {
-            Listener = new ANTListener(response => Console.WriteLine(BitConverter.ToString(response.getDataPayload())));
+            Listener = new ANTListener(this.ANTListenerDelegate);
+        }
+
+        /// <summary>
+        /// Receives ANT messages from ANTListener and passes them off to IPSender.
+        /// </summary>
+        /// <param name="response"></param>
+        public void ANTListenerDelegate(ANT_Managed_Library.ANT_Response response)
+        {
+            Console.WriteLine(BitConverter.ToString(response.getDataPayload()));
+            if (response.isExtended())
+            {
+                ANT_Managed_Library.ANT_ChannelID channelID = response.getDeviceIDfromExt();
+                Console.WriteLine("Device ID: {0:D}-{1:D}-{2:D}", channelID.deviceNumber, channelID.deviceTypeID, channelID.transmissionTypeID);
+            }
         }
 
         /*********************************************************************/

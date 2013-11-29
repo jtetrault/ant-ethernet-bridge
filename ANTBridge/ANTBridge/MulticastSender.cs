488256b6a3c5ebf43dc65a0bf032668c9495ab2c
Joel Tetrault <star99ers@gmail.com>
2013-11-29 15:23:12 -0600

    created MulticastSender class for sending messages as bytes

---
+++ ANTBridge/ANTBridge/MulticastSender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ANTBridge
+{
+    class MulticastSender
+    {
+        /*********************************************************************/
+        /*** Instance Methods ************************************************/
+        /*********************************************************************/
+
+        /// <summary>
+        /// Initialize a UDP connection to a multicast address.
+        /// </summary>
+        /// <param name="multicastAddress">The multicast address to send UDP packets to.</param>
+        /// <param name="port">The port to use for UDP datagrams.</param>
+        public MulticastSender(IPAddress multicastAddress, ushort port)
+        {
+            UdpLink = new UdpClient();
+            UdpLink.JoinMulticastGroup(multicastAddress);
+            EndPoint = new IPEndPoint(multicastAddress, port);
+        }
+
+        public void Send(byte[] message)
+        {
+            UdpLink.Send(message, message.Length, EndPoint);
+        }
+
+
+        /*********************************************************************/
+        /*** Instance Variables **********************************************/
+        /*********************************************************************/
+        /// <summary>
+        /// The UDP link, used to establish a multicast group to send messages to.
+        /// </summary>
+        private UdpClient UdpLink;
+
+        /// <summary>
+        /// The end point (IP address and port) that messages will go to.
+        /// </summary>
+        private IPEndPoint EndPoint;
+    }
+}

f97c420454a626e7137563f98fe8cf163bc92b77
Joel Tetrault <star99ers@gmail.com>
2013-12-01 22:44:44 -0600

    added MulticastListener project for testing

---
+++ ANTBridge/MulticastListener/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 2222);
+            client.Client.Bind(localEp);
+
+            IPAddress multicastAddress = IPAddress.Parse("239.0.0.222");
+            client.JoinMulticastGroup(multicastAddress);
+
+            while (true)
+            {
+                byte[] message = client.Receive(ref localEp);
+                Console.WriteLine("Received " + BitConverter.ToString(message));
+            }
+        }
+    }
+}
